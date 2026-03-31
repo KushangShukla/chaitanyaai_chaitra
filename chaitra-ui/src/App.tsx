3ffccf65b87ce8232d511a99b4fb83966d7f96c5
KushangShukla <kushangashukla1@gmail.com>
2026-03-31 23:12:05 +0530

    Added UI

---
+++ chaitra-ui/src/App.tsx
@@ -0,0 +1,12 @@
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div style={{height:"100vh", background:"#0f172a",color:"white"}}>
+      <h1 style={{padding:"20px"}}>CHAITRA AI</h1>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
