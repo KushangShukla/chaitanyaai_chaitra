d40e09faccbbd22f3f56a28fd67d2bc33bb8db52
KushangShukla <kushangashukla1@gmail.com>
2026-04-02 20:54:40 +0530

    Updated with voice mode

--- chaitra-ui/src/services/voice.ts
+++ chaitra-ui/src/services/voice.ts
@@ -1,3 +1,25 @@
 import axios from "axios";
 
-const ELEVEN_API_KEY=
+const ELEVEN_API_KEY=;
+
+export const speak=async (text:string) => {
+    const response=await axios.post(
+        "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM",
+        {
+            text:text,
+            model_id:"eleven_monolingual_v1",
+        },
+        {
+            headers: {
+                "xi-api-key":ELEVEN_API_KEY,
+                "Content-Type":"application/json",
+            },
+            responseType:"arraybuffer",
+        }
+    );
+    const audioBlob=new Blob ([response.data], {type:"audio/mpeg"});
+    const audioUrl=URL.createObjectURL(audioBlob);
+
+    const audio=new Audio (audioUrl);
+    audio.play();
+};
