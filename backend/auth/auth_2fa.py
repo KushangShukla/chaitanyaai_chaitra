51358069080cb16eb2518212692767c39f5f35f1
KushangShukla <kushangashukla1@gmail.com>
2026-04-09 02:42:40 +0530

    Added authentication via multiple factors

---
+++ backend/auth/auth_2fa.py
@@ -0,0 +1,16 @@
+import pyotp
+import qrcode
+import io
+import base64
+
+def generate_2fa_secret(username):
+    secret=pyotp.random_base32()
+
+    uri=pyotp.totp.TOTP(secret).provisioning_uri(name=username, issuer_name="CHAITRA")
+
+    qr=qrcode.make(uri)
+
+    buffer=io.BytesIO()
+    qr.save(buffer, format="PNG")
+    qr_image=base64.b64encode(buffer.getvalue()).decode()
+    return secret, qr_image
